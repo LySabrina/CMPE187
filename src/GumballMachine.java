00f0b74d839b049722b5917f202372fed4e1c228
LySabrina <lysabrinak@gmail.com>
2023-02-03 16:01:21 -0800

    Added code to tester and gumball

--- src/GumballMachine.java
+++ src/GumballMachine.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class GumballMachine {
 
     //inputtedCurrency used to keep track the amount of coins inserted by user
     private int inputtedCurrency;
+
+    //Coins that are only valid for the gumball machine
+    private final List VALID_COINS = Arrays.asList("dime", "nickel", "quarter");
 
     /**
      * Initialize GumballMachine with 0 coins
@@ -14,29 +21,63 @@ public class GumballMachine {
      * Dispenses Yellow Gumball
      * @return
      */
-    public String dispenseYellow(){
+    public void dispenseYellow(){
         if(inputtedCurrency >= 10){
-            return "";
+            inputtedCurrency -= 10;
+            System.out.println("Dispensed: 1 yellow gumball");
+            System.out.println("Current inputted currency: " + inputtedCurrency);
         }
-        return "Not enough currency. Please input at least 10 cents";
+        else{
+            System.out.println( "Not enough currency. Please input at least 10 cents");
+        }
+
+
     }
 
-    public String dispenseRed(){
+    public void dispenseRed(){
         if(inputtedCurrency >= 5){
-            return "";
+            inputtedCurrency -= 5;
+            System.out.println( "Dispensed: 1 red gumball");
+            System.out.println("Current inputted currency: " + inputtedCurrency);
         }
-        return "Not enough currency. Please inpput at least 5 cents";
+        else{
+            System.out.println( "Not enough currency. Please input at least 5 cents");
+        }
+
+
     }
 
     public void insert(String coin){
-
+        if(!VALID_COINS.contains(coin.toLowerCase())){
+            System.out.println("Returning Invalid " + coin);
+        }
+        else{
+            switch (coin.toLowerCase()){
+                case "quarter":
+                    inputtedCurrency += 25;
+                    System.out.println("Inserted 25 cents");
+                    break;
+                case "nickel":
+                    inputtedCurrency += 5;
+                    System.out.println("Inserted 5 cents");
+                    break;
+                case "dime":
+                    inputtedCurrency +=10;
+                    System.out.println("Inserted 10 cents");
+                    break;
+            }
+        }
     }
 
-    public String returnChange(){
+    public void returnChange(){
         if(inputtedCurrency == 0){
-            return "Return 0 cents";
+            System.out.println("There is no coins inside. Returning 0 cents");
+
         }
-        return "";
+        else{
+            System.out.println("Returning coins: " + String.valueOf(inputtedCurrency));
+            inputtedCurrency = 0;
+        }
     }
 
 }
--- src/MainTester.java
+++ src/MainTester.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class MainTester {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        GumballMachine gumballMachine = new GumballMachine();
+        System.out.println("Welcome to Gumball Machine\n1) Insert Coin 2) Red Lever 3) Yellow Lever 4) Change Lever 5) Exit");
+        while(s.hasNextLine()){
+            String input = s.nextLine();
+            if(input.equals("1")){
+                System.out.println("Enter coin (dime, nickel, quarter):");
+                String coin = s.nextLine();
+                gumballMachine.insert(coin);
+            }
+            else if(input.equals("2")){
+                gumballMachine.dispenseRed();
+            }
+            else if(input.equals("3")){
+                gumballMachine.dispenseYellow();
+            }
+            else if(input.equals("4")){
+                gumballMachine.returnChange();
+            }
+            else if(input.equals("5")){
+                System.out.println("Goodbye!");
+                break;
+            }
+            System.out.println("1) Insert Coin 2) Red Lever 3) Yellow Lever 4) Change Lever 5) Exit");
+        }
     }
 }
